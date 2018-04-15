c884c6fdf5def2cfff6f5697c853ce6694110981
BelkinAndrey <it.belkin@gmail.com>
2018-04-15 14:38:55 +0700

    Add files via upload

---
+++ OpenTadpole/Assets/LiquidPhysics2D/DataStructs/LPContact.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//This class contains information about a fixture / fixture contact
+public struct LPContactFixFix
+{
+	public int BodyAIndex;
+	public int FixtureAIndex;
+	
+	public int BodyBIndex;
+	public int FixtureBIndex;
+	
+	public Vector3 ManifoldPoint1;
+	public Vector3 ManifoldPoint2;
+	
+	public Vector3 Normal;
+	
+	public bool IsTouching;
+}
+
+//This class contains information about a particle / fixture contact
+public struct LPContactPartFix
+{
+	public int ParticleSystemIndex;
+	public int ParticleIndex;
+	
+	public int BodyIndex;
+	public int FixtureIndex;
+		
+	public Vector3 Normal;
+}
+
+//This class contains information about a particle / particle contact
+public struct LPContactPartPart
+{
+	public int ParticleSystemIndex;
+	public int ParticleAIndex;
+	public int ParticleBIndex;	
+}
---
+++ OpenTadpole/Assets/LiquidPhysics2D/DataStructs/LPRayCastHit.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//This class contains information about a raycast hitting a fixture
+public struct LPRayCastHit
+{
+	//Indicates whether the raycast hit has hit a fixture or a particle.
+	public LPRayCastHitType WhatDidIHit;
+	//Indicates the index number of the fixtures body or the particles particle system
+	public int ParentIndex;
+	public Vector3 Position;
+	public Vector3 Normal;
+	//Indicates the fixtures index or the index of the particle in its particle system 
+	public int Index;
+	public float Fraction;
+}
+
